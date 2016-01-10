f9ba4e23cf58927351e4e0e8e67fc4177aec3cad
Carl Deboer <cdeboer@behemoth.ccbr.utoronto.ca>
2016-01-10 16:37:53 -0500

    Fixed superZ to work properly with doubleStranded mode - also modified that section to use a hash of k+1-mers and only take max/min superZs at the end. Finally, added a unit test for testing doubleStranded that should always have subZs and superZs equal to 0 becuase BG=RC(FG).

--- runTests.bat
+++ runTests.bat
@@ -1,10 +1,13 @@
 #/bin/bash
 
-AMUSED -s 6 -q test/test1.fa -o test1.res.gz
+AMUSED -s 6 -q test/test1.fa -o test1.res.gz -do
 echo "test1: " `zdiff test1.res.gz test/test1.s6.results.gz | wc -l` " differences (should be 0)"
 
-AMUSED -ds -s 6 -q test/test1.fa -o test2.res.gz
+AMUSED -ds -s 6 -q test/test1.fa -o test2.res.gz -do
 echo "test2: " `zdiff test2.res.gz test/test2.s6.results.gz | wc -l` " differences (should be 0)"
 
-AMUSED -s 6 -b test/test2.fa.gz  -q test/test1.fa -o test3.res.gz
+AMUSED -s 6 -b test/test2.fa.gz  -q test/test1.fa -o test3.res.gz -do
 echo "test3: " `zdiff test3.res.gz test/test3.s6.results.gz | wc -l` " differences (should be 0)"
+
+AMUSED -s 6 -b test/test4.fa  -q test/test4_RC.fa -o test4.res.gz -do  -bc -ds
+echo "test4: " `zdiff test4.res.gz test/test4.s6.results.gz | wc -l` " differences (should be 0)"
